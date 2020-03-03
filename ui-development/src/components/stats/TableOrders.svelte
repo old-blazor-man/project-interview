890f90cd05f96b6c998ba5584985f1c9b41f188e
lrgvdc9-1-1 <hchapa@lrgvdc911.org>
2020-03-03 15:13:25 -0600

    awesome changes

--- ui-development/src/components/stats/TableOrders.svelte
+++ ui-development/src/components/stats/TableOrders.svelte
@@ -36,7 +36,9 @@
                 <td>{order_name}</td>
                 <td>{order_total}</td>
                 <td>{order_date}</td>
-                <td><button on:click="{()=>{removeOrder(i)}}">Delete</button></td>
+                <td><button on:click="{()=>{removeOrder(i)}}">Delete</button>
+                    <button>Edit Item</button>
+                </td>
             </tr>
         {/each}
         
